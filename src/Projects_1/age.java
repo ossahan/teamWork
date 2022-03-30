6fdc76151a27a76e4ef32818deb292900477e4d6
Ustun Ertas <ustunertas0@gmail.com>
2022-03-30 22:34:01 +0200

    else iflerle sartlari girdim ve bitirdim

--- src/Projects_1/age.java
+++ src/Projects_1/age.java
@@ -32,7 +32,7 @@ public class age {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("yasinizi giriniz : ");
+        System.out.println("Yasinizi giriniz : ");
         int myAge = scanner.nextInt();
 
         if (myAge <=10){
@@ -41,16 +41,16 @@ public class age {
         }else if (myAge <=16){
             System.out.println("Ehliyet almak icin cok gencsin");
 
+        }else if (myAge<=18){
+            System.out.println("Dovme yaptirmak icin cok gencsin");
+
+        }else if (myAge<=20){
+            System.out.println("Alkol icmek icin cok gencsin");
+
+        }else if (myAge>=21){
+
+            System.out.println("Istedigini yapabilirsin");
         }
-
-        //  code Start here don't change before this line
-
-        //System.out.println("too young to drink alcohol");
-
-        // kodu burdan baslatin ve bu satirdan onceki kodlari degistirmeyin
-
-
-        
 
     }
 }
